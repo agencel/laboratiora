e7fb2711d1d7aaa92b17fe4b82b159e15b32bb3c
agencel <agencel@sigma.ug.edu.pl>
2016-01-15 23:21:40 +0100

    Update 2.12.c

--- 2.12.c
+++ 2.12.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include "rect.h"
 
+int main()
+{
+  struct rect r1 = {{1, 1}, {2, 2}};
+  struct rect r2 = {{1, 1}, {4, 4}};
+
+  int a = disjointrect(r1,r2);
+  printf("%d\n",a);
+}
